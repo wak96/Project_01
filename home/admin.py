b0d8d25fb4976f884b76c0d3512bdc21136183f0
walidayankawsar <walidayankawsar@gmail.com>
2025-11-11 05:18:46 -0800

    registered in admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import Clients, Work, Services, Profile
 # Register your models here.
+
+admin.site.register(Clients)
+admin.site.register(Work)
+admin.site.register(Services)
+admin.site.register(Profile)
+
