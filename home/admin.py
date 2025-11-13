f77280f5ef8ec0753fbcdcc645194298293e2d19
walidayankawsar <walidayankawsar@gmail.com>
2025-11-13 00:51:46 -0800

    registered social

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from . models import Clients, Work, Services, Profile
+from . models import Clients, Work, Services, Profile, Social
 # Register your models here.
 
 admin.site.register(Clients)
 admin.site.register(Work)
 admin.site.register(Services)
 admin.site.register(Profile)
+admin.site.register(Social)
 
